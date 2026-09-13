2307709f0aaafe35a301b496b4befafde7b72957
Matias Kruk <kruk.matias@gmail.com>
2026-09-13 05:02:04 +0200

    chore(core): expose core modules

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export * from './canonical.ts';
+export * from './actions.ts';
+export * from './state-machine.ts';
+export * from './signing.ts';
+export * from './challenge.ts';
+export * from './evidence.ts';
+export * from './receipts.ts';
